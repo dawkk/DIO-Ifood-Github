68e09a9f5d4d11b245f576d080fc9f1cd2c5ce72
dawkk <gventuri14@gmail.com>
2022-10-17 11:21:49 -0300

    Adiciona Métodos e exercicio smartTv

---
+++ java/java-basico/estudos/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       
+        SmartTv smartTv = new SmartTv();
+
+        
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.mudarCanal(15);
+        smartTv.diminuirCanal();
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada ? " + smartTv.ligada);
+
+        
+    }
+}
